be712492575aaef2a7f3446baf877cdcc004d66c
trystonmcgee <trystonmcgee@gmail.com>
2026-07-14 21:17:55 -0500

    Add: maximum-score-after-splitting-a-string - submission-7

---
+++ Data Structures & Algorithms/maximum-score-after-splitting-a-string/submission-7.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxScore(self, s: str) -> int:
+        res = 0
+        idx = 0
+        while idx < len(s) - 1:
+            left = s[:idx + 1].count("0")
+            right = s[idx + 1:].count("1")
+            if left + right > res:
+                res = left + right
+
+            idx += 1
+
+        return res
