8780c43ee4337ce5ec916bf60302a40e088b6c07
trystonmcgee <trystonmcgee@gmail.com>
2026-07-15 02:34:55 -0500

    Add: missing-number - submission-5

---
+++ Data Structures & Algorithms/missing-number/submission-5.py
@@ -0,0 +1,20 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        ran = [num for num in range(0, len(nums) + 1)]
+        cnts = {}
+
+        for num in nums:
+            if num not in cnts:
+                cnts[num] = 1
+            else:
+                cnts[num] += 1
+        
+        for num in ran:
+            if num not in cnts:
+                cnts[num] = 1
+            else:
+                cnts[num] += 1
+        
+        for num, cnt in cnts.items():
+            if cnt == 1:
+                return num
