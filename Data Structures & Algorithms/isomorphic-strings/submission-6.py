9ba4667f01ab08f55df5241825dff4fa9c7c716b
trystonmcgee <trystonmcgee@gmail.com>
2026-08-04 17:07:58 -0500

    Add: isomorphic-strings - submission-6

---
+++ Data Structures & Algorithms/isomorphic-strings/submission-6.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        table = {}
+        for i in range(len(s)):
+            if s[i] not in table.keys() and t[i] not in table.values():
+                table[s[i]] = t[i]
+        
+        print(table)
+        new_s = ""
+        for char in s:
+            if char not in table:
+                return False
+            
+            new_s += table[char]
+    
+        return new_s == t 
